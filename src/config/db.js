cc5b554c9d6551b29a449592360234e306cd4673
Tejas Shastri <tejas.shastri@apptware.com>
2026-04-01 13:59:14 +0530

    Wishlist functionality and other changes

--- src/config/db.js
+++ src/config/db.js
@@ -2,16 +2,16 @@ import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 dotenv.config();
 
-// const pool = mysql.createPool(process.env.MYSQL_URL);
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
+const pool = mysql.createPool(process.env.MYSQL_URL);
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0
+// });
 // Test connection
 pool.getConnection()
   .then(connection => {
